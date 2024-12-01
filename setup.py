4e65b04ef20728457731bbd199250627f77cc745
Byeongdulee <45212115+Byeongdulee@users.noreply.github.com>
2024-11-30 18:43:47 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
    version='1.0.0',
    author='Byeongdu Lee',
    author_email='blee@anl.gov',
-   packages=['SmaractStage'],
+#   packages=['SmaractStage'],
    url='#',
    license='LICENSE.txt',
    description='APS 12ID smaract stage control',
-   install_requires=[
-       "SmarAct SDK",
-   ],
+#   install_requires=[
+#       "SmarAct SDK",
+#   ],
 )
